050e50eb784bc0cbf1faa4848d6d1ead0e6073da
GuillermoJCV <memo92975@gmail.com>
2024-06-01 16:06:47 -0600

    Añade eñ primer ejercicio

---
+++ adventus-js-1st-exercise/INSTRUCTIONS.md
@@ -0,0 +1,13 @@
+Esta prueba fue sacada de [AdventusJS](https://adventjs.dev/es#retos)
+
+1ra prueba<br>
+
+# ¡Primer Regalo repetido!
+
+En la fábrica de juguetes del Polo Norte, cada juguete tiene un número de **identificación único**.<br/>
+
+Sin embargo, debido a un error en la máquina de juguetes, algunos números se han asignado a más de un juguete.<br/>
+
+¡Encuentra el **primer número de identificación que se ha repetido**, donde la **segunda ocurrencia tenga el índice más pequeño!**<br/>
+
+En otras palabras, si hay más de un número repetido, debes **devolver el número cuya segunda ocurrencia aparezca primero en la lista**. **Sino**, hay números repetidos, **devuelve -1**.<br/>
---
+++ adventus-js-1st-exercise/index.js
@@ -0,0 +1,36 @@
+import { test } from "../utils/test.js"
+
+function findFirstRepeated(gifts) {
+  const resultSet = new Set([...gifts])
+
+  for(const gift of gifts) {
+  	if(!resultSet.delete(gift)) {
+  		return gift
+  	}
+  }
+
+  return -1
+}
+
+
+const giftIds1 = [2, 1, 3, 5, 3, 2]
+const result1 = findFirstRepeated(giftIds1)
+const expected1 = 3
+test(1, result1, expected1)
+
+// 3
+// Aunque el 2 y el 3 se repiten
+// el 3 aparece primero por segunda vez
+
+const giftIds2 = [1, 2, 3, 4]
+const result2 = findFirstRepeated(giftIds2)
+const expected2 = -1
+test(2, result2, expected2)
+// -1
+// Es -1 ya que no se repite ningún número
+
+const giftIds3 = [5, 1, 5, 1]
+const result3 = findFirstRepeated(giftIds3)
+const expected3 = 5
+test(3, result3, expected3)
+// 5

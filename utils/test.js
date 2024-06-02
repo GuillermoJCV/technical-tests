8e6376041ff09331793313ff28d16564bce26fa6
GuillermoJCV <memo92975@gmail.com>
2024-06-02 15:36:24 -0600

    Añade una manera de ver el tiempo de ejecución de cada test

--- utils/test.js
+++ utils/test.js
@@ -1,13 +1,16 @@
 import assert from 'node:assert';
 
 function test(id, input, expected) {
+	console.time("Test time")
 	try {
 		assert.equal(input, expected, `Received "${input}" and it was expected "${expected}"`)
 		console.log("\x1b[32m", `${id} passed : input:"${input}" expected:"${expected}"`)
 	} catch(err) {
 		console.log("\x1b[31m", `${id} : ${err}`)
 	}
-	console.log("\x1b[37m")
+	console.log("\x1b[0m")
+	console.timeEnd("Test time")
+
 }
 export { 
 	test 

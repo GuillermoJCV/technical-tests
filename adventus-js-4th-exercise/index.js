a22444b48bd9cce47387a724339789d28cdf6af5
GuillermoJCV <memo92975@gmail.com>
2024-06-04 18:12:55 -0600

    Añade un archivo que no se quiso hacer commit

--- adventus-js-4th-exercise/index.js
+++ adventus-js-4th-exercise/index.js
@@ -2,17 +2,22 @@ import { test } from "../utils/test.js"
 
 function decode(message) {
 	const open = message.indexOf("(")
-	const close = message.indexOf(")")
+	let close = message.indexOf(")")
+
+	if(message.indexOf("(", open) < close) {
+		close = message.indexOf(")", close)
+	}
+
 	const before = message.substring(0, open)
 	const after = message.substring(close + 1)
 	let result = message
 
-	if(open !== -1) {
+	if(open !== -1 && close !== -1) {
 		let newMessage = result.substring(open + 1, close)
 		const newMessageArr = newMessage.split("")
 		newMessageArr.reverse()
 		newMessage = newMessageArr.join("")
-		return before + newMessage + after
+		return decode(before + newMessage + after)
 	} else {
 		return result
 	}
@@ -26,12 +31,12 @@ function decode(message) {
 /*______________________________2______________________________*/
 	const result2 = decode('(olleh) (dlrow)!')
 	const expected2 = "hello world!"
-	test("First", result2, expected2)
+	test("Second", result2, expected2)
 
 /*______________________________3______________________________*/
 	const result3 = decode('sa(u(cla)atn)s')
 	const expected3 = "santaclaus"
-	test("First", result3, expected3)
+	test("Third", result3, expected3)
 
 // Paso a paso:
 // 1. Invertimos el anidado -> sa(ualcatn)s
